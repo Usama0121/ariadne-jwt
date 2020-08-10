b1778fcc180e769181ec7f06201c131d06ca81d7
Usama0121 <aslam0121@gmail.com>
2020-08-10 23:37:42 +0500

    Updated example project

--- examples/project_1/user/mutations.py
+++ examples/project_1/user/mutations.py
@@ -1,6 +1,13 @@
 from ariadne import MutationType
 
-from ariadne_jwt import resolve_verify, resolve_refresh, resolve_token_auth
+from ariadne_jwt import resolve_verify, resolve_refresh
+from ariadne_jwt.decorators import token_auth
+
+
+@token_auth
+def resolve_token_auth(obj, info, **kwargs):
+    return {'user': info.context.get('request').user}
+
 
 mutation = MutationType()
 
