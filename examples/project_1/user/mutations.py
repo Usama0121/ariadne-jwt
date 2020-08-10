0f56a8273e96cb133ae3a8d79146b2de4131e1ec
Usama0121 <aslam0121@gmail.com>
2020-08-10 22:14:09 +0500

    Updated example project

--- examples/project_1/user/mutations.py
+++ examples/project_1/user/mutations.py
@@ -1,24 +1,9 @@
 from ariadne import MutationType
-from django.contrib.auth import authenticate, login
 
-from ariadne_jwt import resolve_verify, resolve_refresh
-from ariadne_jwt.shortcuts import get_token
+from ariadne_jwt import resolve_verify, resolve_refresh, resolve_token_auth
 
 mutation = MutationType()
 
 mutation.set_field('verifyToken', resolve_verify)
 mutation.set_field('refreshToken', resolve_refresh)
-
-
-@mutation.field('LogIn')
-def resolve_login(obj, info, **kwargs):
-    user = authenticate(**kwargs)
-
-    if user is None:
-        raise Exception('Please enter a correct username and password')
-
-    if not user.is_active:
-        raise Exception('It seems your account has been disabled')
-
-    login(info.context.get('request'), user)
-    return {'token': get_token(user)}
+mutation.set_field('tokenAuth', resolve_token_auth)
