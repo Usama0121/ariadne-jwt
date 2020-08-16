ba54ef5dfe758b03d5e71d5b50c27792f0c1d5eb
Usama0121 <aslam0121@gmail.com>
2020-08-16 22:59:44 +0500

    Added refresh token models test

--- tests/context_managers.py
+++ tests/context_managers.py
@@ -6,6 +6,14 @@ from ariadne_jwt.settings import jwt_settings
 
 
 @contextmanager
+def catch_signal(signal):
+    handler = mock.Mock()
+    signal.connect(handler)
+    yield handler
+    signal.disconnect(handler)
+
+
+@contextmanager
 def back_to_the_future(**kwargs):
     with mock.patch('ariadne_jwt.utils.datetime') as datetime_mock:
         datetime_mock.utcnow.return_value = datetime.utcnow() + timedelta(**kwargs)
